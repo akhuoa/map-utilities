da47eadb7480534afc32ec75cb82d7f023b10ae2
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2024-08-21 13:10:43 +1200

    Update export components

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,14 @@ import DrawToolbar from "./DrawToolbar/DrawToolbar.vue";
 import HelpModeDialog from "./HelpModeDialog/HelpModeDialog.vue";
 import Tooltip from "./Tooltip/Tooltip.vue";
 import TreeControls from "./TreeControls/TreeControls.vue";
-import IframeImageDialog from "./ImageDialog/IframeImageDialog.vue";
 import CopyToClipboard from "./CopyToClipboard/CopyToClipboard.vue";
+import IframeImageDialog from "./ImageDialog/IframeImageDialog.vue";
 
-export { DrawToolbar, HelpModeDialog, Tooltip, TreeControls, CopyToClipboard, IframeImageDialog };
+export {
+  DrawToolbar,
+  HelpModeDialog,
+  Tooltip,
+  TreeControls,
+  CopyToClipboard,
+  IframeImageDialog
+};
