b9a882deb6dc0baa463ba1f71c0ac9ab20706290
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2026-03-10 13:26:20 +1300

    Update direct match  style

--- src/components/ConnectivityList/ReconciliationTable.vue
+++ src/components/ConnectivityList/ReconciliationTable.vue
@@ -222,14 +222,14 @@ export default {
 
 	// Direct match merged cell styling (one-to-one exact match)
 	&.direct-match-cell {
-		&::before {
-			background-color: #e3f2fd;
-			border-left: 4px solid #90caf9;
+    &::before {
+			background-color: #e6ffed; // #e3f2fd;
+			border-left: 4px solid #aceebb; // #90caf9;
 		}
 
 		&:hover::before {
-			background-color: #bbdefb;
-			border-left-color: #64b5f6;
+			background-color: #d9ffe0; // #bbdefb;
+			border-left-color: #7fe09c; // #64b5f6;
 		}
 	}
 
